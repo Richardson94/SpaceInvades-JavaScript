81cd2c44bce7cfa526bb0101c2b1fa9bad69857a
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-07-29 20:58:06 -0400

    base and dependences for all project

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log(`El valor de la suma es ${Sum(2,3)}`);
+
+export function Sum(a, b ){
+  return a+b;
+}
---
+++ main.test.js
@@ -0,0 +1,6 @@
+import {Sum} from './main';
+describe('Testing coverage',()=>{
+test('test inside describe',()=>{
+  expect(Sum(2,3)).toBe(5);
+});
+});
