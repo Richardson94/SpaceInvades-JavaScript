5454a9c2ddf0a0e3c0f34116e4996d87a600a130
Richardson94 <richardcori94@gmail.com>
2023-04-12 13:20:50 -0400

    test push

--- unit-test/main.test.js
+++ unit-test/main.test.js
@@ -4,13 +4,4 @@ describe('Testing coverage',()=>{
 it('test one',()=>{
   expect(5).toBe(5);
 });
-it('test Two',()=>{
-  expect(1).toBe(1);
-});
-it('test Three',()=>{
-  expect('asdf').toBe('asdf');
-});
-it('test Four',()=>{
-  expect("asdfff").toBe("asdfff");
-});
 });
